55867b0813ea57bde0cc2343dfb90c998330a401
Jack Cook <hello@jackcook.nyc>
2017-03-25 18:35:08 -0400

    Added unit test for 2-proportion Z-interval

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,4 @@ using Base.Test
 include("../src/calculations.jl")
 
 @test one_prop_z_int(0.5, 100, 0.95) == ConfidenceInterval(0.402, 0.598)
+@test two_prop_z_int(0.5, 100, 0.7, 200, 0.95) == ConfidenceInterval(-0.3168, -0.0832)
