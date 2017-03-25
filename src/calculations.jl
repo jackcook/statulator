4323826c6b9f1068e049143879aae5f527516ea4
joycezhang229 <zhangj8@bxscience.edu>
2017-03-25 16:24:53 -0400

    start calc for one prop z test

--- src/calculations.jl
+++ src/calculations.jl
@@ -19,10 +19,19 @@ function two_prop_z_int(p1, p2, n1, n2, lvl)
     standard_normal = Normal()
     critical_value = quantile(standard_normal, lvl + (1 - lvl) / 2)
 
-    lowest_value = round(p - critical_value * standard_error, 4)
-    highest_value = round(p + critical_value * standard_error, 4)
+    lowest_value = round((p1-p2) - critical_value * standard_error, 4)
+    highest_value = round((p1-p2) + critical_value * standard_error, 4)
 
     println("The interval is (", lowest_value, ", ", highest_value, ")")
     println("In other words, You are", lvl "percent likely to have the true
     proportion difference lie between", lowest_value "and" highest_value)
 end
+
+function one_prop_z_test(p, psample, n, alpha, prop)
+    standard_error = sqrt((p * (1-p))) / n)
+
+    standard_normal = Normal()
+    critical_value = quantile(standard_normal, (psample - p)/ standard_error)
+
+    probability = 
+    println
