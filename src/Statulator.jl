61171e9b1eceb16000f4e7b8c90bc7a68700e281
Jack Cook <hello@jackcook.nyc>
2017-03-25 16:07:12 -0400

    where'd my code go

--- src/Statulator.jl
+++ src/Statulator.jl
@@ -7,9 +7,28 @@ function input(prompt::AbstractString="")
     return chomp(readline())
 end
 
-q1 = input("Is this is a problem of sample proportions or means? [p/m] ")
+function terminate(message::AbstractString="")
+    println(message)
+    exit()
+ end
+
+ q1 = input("Is this is a problem of sample proportions or means? [p/m] ")
+
+if q1 != "p" && q1 != "m"
+    terminate("You must enter 'p' for proportion or 'm' for mean.")
+end
+
 q2 = input("Do you want to run a test or create an interval? [t/i] ")
+
+if q2 != "t" && q2 != "i"
+    terminate("You must enter 't' for test or 'i' for interval.")
+end
+
 q3 = input("How many proportions are you working with? [1/2] ")
+
+if q3 != "1" && q3 != "2"
+    terminate("Only one and two proportion tests and intervals are currently supported.")
+end
 
 if q1 == "p"
     if q2 == "t"
@@ -20,16 +39,19 @@ if q1 == "p"
             alpha =parse(Float64,input("What is your alpha level?"))
             prop= parse(Float64, input("Is your Alternate hypothesis '!=','>', or '<'?"))
             one_prop_z_test(p,psample,n,alpha,prop)
-
         elseif q3 == "2"
             println("2-proportion Z-test")
         end
     elseif q2 == "i"
         if q3 == "1"
-            p = parse(Float64, input("What is your sample proportion? "))
-            n = parse(Float64, input("What is your sample size? "))
-            lvl = parse(Float64, input("What is your needed confidence level? (in proportion) "))
-            one_prop_z_int(p, n, lvl)
+            try
+                p = parse(Float64, input("What is your sample proportion? "))
+                n = parse(Float64, input("What is your sample size? "))
+                lvl = parse(Float64, input("What is your needed confidence level? (in proportion) "))
+                one_prop_z_int(p, n, lvl)
+            catch
+                println("You must enter a numeric value.")
+            end
         elseif q3 == "2"
             p1 = parse(Float64, input("What is your first sample proportion? "))
             n1 = parse(Float64, input("What is your first sample size? "))
